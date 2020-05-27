4bf2fc7dc6e84e84c50101997c4c71223ed45e7e
MinhTu <minhtunguyenphan@gmail.com>
2020-05-27 13:19:35 +0700

    feat: adding model

---
+++ server/src/models/address.model.ts
@@ -0,0 +1,56 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {User, UserWithRelations} from './user.model';
+
+@model()
+export class Address extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  streetName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  ward: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  district: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  city: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  country: string;
+
+  @belongsTo(() => User, {name: 'address'})
+  userId?: string;
+
+  constructor(data?: Partial<Address>) {
+    super(data);
+  }
+}
+
+export interface AddressRelations {
+  // describe navigational properties here
+  user: UserWithRelations
+}
+
+export type AddressWithRelations = Address & AddressRelations;
---
+++ server/src/models/user.model.ts
@@ -0,0 +1,36 @@
+import {Entity, model, property, hasOne} from '@loopback/repository';
+import {Address} from './address.model'
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  username: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @hasOne(() => Address, {keyTo: 'userId'})
+  address?: Address;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
